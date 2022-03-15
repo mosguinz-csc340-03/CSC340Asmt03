fd86039f21964def4f102bdc1b8e9946be7df803
mosguinz <mos.guinz@gmail.com>
2022-03-15 10:43:55 +0700

    Add function to print arg parse error

--- part_c/DictClient.cpp
+++ part_c/DictClient.cpp
@@ -1,1 +1,3 @@
 #include "DictClient.h"
+
+const std::vector<std::string> DictClient::QUERY_ARGS{"a part of speech", "'distinct'", "'unique'"};
--- part_c/DictClient.h
+++ part_c/DictClient.h
@@ -14,6 +14,9 @@ class DictClient {
     unsigned int query_count = 0;
 
  public:
+
+    static const std::vector<std::string> QUERY_ARGS;
+
     static void PrintToConsole(const std::vector<std::string> &messages) {
         std::cout << std::setw(8) << '|' << '\n';
         for (const std::string &message : messages) {
@@ -56,6 +59,52 @@ class DictClient {
             );
         }
         PrintToConsole(res);
+    }
+
+    static void PrintParsingError(int arg_index, const std::string &arg) {
+        std::vector<std::string> query_options = QUERY_ARGS;
+        std::vector<std::string> messages;
+        messages.reserve(query_options.size() + 2);
+
+        std::string arg_ordinal;
+        switch (arg_index) {
+            case 0: arg_ordinal = "1st";
+                break;
+            case 1: arg_ordinal = "2nd";
+                break;
+            case 2: arg_ordinal = "3rd";
+                break;
+            default: arg_ordinal = std::to_string(arg_index + 1) + "th";
+        }
+
+        for (const std::string &option : query_options) {
+            messages.emplace_back(std::string("<The entered ")
+                                      .append(arg_ordinal)
+                                      .append(" parameter is NOT ")
+                                      .append(option)
+                                      .append(".>"));
+        };
+
+        messages.emplace_back(std::string("<The entered ")
+                                  .append(arg_ordinal)
+                                  .append(" parameter '")
+                                  .append(arg)
+                                  .append("' was disregarded.>"));
+
+        std::string arg_list;
+        for (const std::string &option : query_options) {
+            if (!arg_list.empty()) {
+                arg_list.append(" or ");
+            }
+            arg_list.append(option);
+        }
+        messages.emplace_back(std::string("<The ")
+                                  .append(arg_ordinal)
+                                  .append(" parameter should be ")
+                                  .append(arg_list)
+                                  .append(".>"));
+
+        PrintToConsole(messages);
     }
 
     std::string PromptInput() {
