7f286919599c03ce92535a5a6e4f7bc1de3e07fd
mosguinz <mos.guinz@gmail.com>
2022-03-16 09:31:56 +0700

    Remove unused code from driver file

--- part_c/Assignment-03_PC_Dictionary.cpp
+++ part_c/Assignment-03_PC_Dictionary.cpp
@@ -1,14 +1,5 @@
-#include <iostream>
-#include <fstream>
-#include "Dictionary.h"
 #include "DictClient.h"
-#include <queue>
-
-int queryCount = 0;
 
 int main() {
-
-    DictClient client;
-    client.StartSession();
-
+    DictClient().StartSession();
 }
